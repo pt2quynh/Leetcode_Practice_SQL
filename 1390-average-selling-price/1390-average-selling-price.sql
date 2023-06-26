171d7188427a6b9d6f87cb7ab3d563635e7b4281
Quynh Pham <93593303+ttqp2812fi@users.noreply.github.com>
2023-06-26 17:05:25 +0700

    Time: 1540 ms (69.85%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
 with t1 as (
         select prices.product_id, prices.start_date, prices.end_date, prices.price, 
-                unitssold.purchase_date, unitssold.units
+        unitssold.purchase_date, unitssold.units
         from prices 
         join UnitsSold on (prices.product_id = unitssold.product_id) 
-        where unitssold.purchase_date between prices.start_date and prices.end_date
+        and(unitssold.purchase_date between prices.start_date and prices.end_date)
 )
 select product_id, round(sum(price*units)/sum(units),2) as average_price 
 from t1
