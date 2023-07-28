b17c51d604431a37f80e206fe12edd73c673b918
Quynh Pham <93593303+ttqp2812fi@users.noreply.github.com>
2023-07-28 14:01:07 +0700

    Time: 2122 ms (35.49%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,9 +1,9 @@
 with t1 as (
     select product_id, year, quantity, price, 
-            rank() over(partition by product_id order by year) as rno
+            rank() over(partition by product_id order by year) as rnk
     from sales
 )
 
 select product_id, year as first_year, quantity, price
 from t1
-where rno = 1
+where rnk = 1
