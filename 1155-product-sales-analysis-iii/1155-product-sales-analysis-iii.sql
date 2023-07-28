0e9b72d9a7de50ee190ee9736a654be0ee6e1515
Quynh Pham <93593303+ttqp2812fi@users.noreply.github.com>
2023-07-28 14:23:21 +0700

    Time: 3331 ms (5%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,9 +1,21 @@
+# solution 1
+# with t1 as (
+#     select product_id, year, quantity, price, 
+#             rank() over(partition by product_id order by year) as rnk
+#     from sales
+# )
+
+# select product_id, year as first_year, quantity, price
+# from t1
+# where rnk = 1
+# --------------------------------------------
+# solution 2
 with t1 as (
-    select product_id, year, quantity, price, 
-            rank() over(partition by product_id order by year) as rnk
+    select product_id, min(year) as first_year
     from sales
+    group by 1
 )
 
-select product_id, year as first_year, quantity, price
-from t1
-where rnk = 1
+select s.product_id, s.year as first_year, s.quantity, s.price
+from sales s
+join t1 on s.product_id = t1.product_id and s.year = t1.first_year
