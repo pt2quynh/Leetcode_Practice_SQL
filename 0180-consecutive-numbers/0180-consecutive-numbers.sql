063d3eed58406f350b7cffdc33576d6409449675
Quynh Pham <93593303+ttqp2812fi@users.noreply.github.com>
2023-07-31 18:06:22 +0700

    Time: 3085 ms (5%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,11 +1,22 @@
-with t1 as (
-        select l1.id as l1_id, l1.num as l1_num,
-        l2.id as l2_id, l2.num as l2_num,
-        l3.id as l3_id, l3.num as l3_num
-        from logs l1, logs l2, logs l3
-        where l1.num = l2.num and l1.id + 1 = l2.id 
-        and l2.num = l3.num and l2.id + 1 = l3.id
-)
+# solution 1
+# with t1 as (
+#         select l1.id as l1_id, l1.num as l1_num,
+#         l2.id as l2_id, l2.num as l2_num,
+#         l3.id as l3_id, l3.num as l3_num
+#         from logs l1, logs l2, logs l3
+#         where l1.num = l2.num and l1.id + 1 = l2.id 
+#         and l2.num = l3.num and l2.id + 1 = l3.id
+# )
 
-select distinct(l1_num) as ConsecutiveNums 
-from t1
+# select distinct(l1_num) as ConsecutiveNums 
+# from t1
+
+# solution 2
+select distinct num as ConsecutiveNums 
+from (
+        select num, 
+                lead(num, 1) over() as lead_1,
+                lead(num, 2) over() as lead_2
+        from logs
+) x
+where (num = lead_1) and(num = lead_2)
