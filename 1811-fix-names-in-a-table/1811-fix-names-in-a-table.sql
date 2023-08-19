758d320df1e1ad7594736d39e3caa45ddc589404
Quynh Pham <93593303+ttqp2812fi@users.noreply.github.com>
2023-08-19 18:14:00 +0700

    Time: 1524 ms (25.75%), Space: 0B (100%) - LeetHub

--- 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
@@ -1,9 +1,9 @@
-# select user_id,
-#         concat(upper(left(name, 1)), lower(right(name, length(name) - length(left(name, 1))))) as name
-# from users
-# order  by 1
-
 select user_id,
-        concat(upper(left(name, 1)), lower(substring(name, 2, length(name) - length(left(name, 1))))) as name
+        concat(upper(left(name, 1)), lower(right(name, length(name) - length(left(name, 1))))) as name
 from users
-order by 1
+order  by 1
+
+# select user_id,
+#         concat(upper(left(name, 1)), lower(substring(name, 2, length(name) - length(left(name, 1))))) as name
+# from users
+# order by 1
