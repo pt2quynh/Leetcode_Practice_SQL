72e6ed3c6df60959a8ca09fb560856756250471e
Quynh Pham <93593303+ttqp2812fi@users.noreply.github.com>
2023-09-17 14:20:19 +0700

    Time: 1476 ms (93.47%), Space: 0B (100%) - LeetHub

--- 1734-bank-account-summary-ii/1734-bank-account-summary-ii.sql
+++ 1734-bank-account-summary-ii/1734-bank-account-summary-ii.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
-select u.name, sub_tb.balance
+# select u.name, sub_tb.balance
+# from users u
+# left join (select account, sum(amount) as balance from transactions group by 1) sub_tb on u.account = sub_tb.account
+# where  sub_tb.balance > 10000
+
+
+select u.name, coalesce(sum(t.amount), 0) as balance
 from users u
-left join (select account, sum(amount) as balance from transactions group by 1) sub_tb on u.account = sub_tb.account
-where  sub_tb.balance > 10000
+left join transactions t on u.account = t.account
+group by 1
+having sum(t.amount) > 10000
